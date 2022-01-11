292a9703ee951727441ee2754e1ad41579680147
Phan Tấn Tài <phantantai9800@gmail.com>
2022-01-11 16:02:43 +0700

    tai

--- packages/make-styles/src/types.ts
+++ packages/make-styles/src/types.ts
@@ -11,101 +11,39 @@ type MakeStylesCSSProperties = Omit<
 export type MakeStylesStrictCSSObject = MakeStylesCSSProperties &
   MakeStylesCSSPseudos & { animationName?: MakeStylesAnimation | MakeStylesAnimation[] | CSS.AnimationProperty };
 
-type MakeStylesCSSObjectCustom = {
-  [Property: string]: MakeStylesStyle | string | 0;
+type MakeStylesCSSPseudos = {
+  [Property in CSS.Pseudos]?:
+    | (MakeStylesStrictCSSObject & { content?: string })
+    | (MakeStylesCSSObjectCustomL1 & { content?: string });
 };
-type MakeStylesCSSPseudos = { [Property in CSS.Pseudos]?: MakeStylesStrictCSSObject & { content?: string } };
 
-export type MakeStylesAnimation = Record<'from' | 'to' | string, MakeStylesCSSObjectCustom>;
-export type MakeStylesStyle = MakeStylesStrictCSSObject | MakeStylesCSSObjectCustom;
+//
+// "MakeStylesCSSObjectCustom*" is a workaround to avoid circular references in types that are breaking TS <4.
+// Once we will support "typesVersions" (types downleleving) or update our requirements for TS this should be
+// updated or removed.
+//
+
+type MakeStylesCSSObjectCustomL1 = {
+  [Property: string]: MakeStylesCSSValue | undefined | MakeStylesCSSObjectCustomL2;
+} & MakeStylesStrictCSSObject;
+type MakeStylesCSSObjectCustomL2 = {
+  [Property: string]: MakeStylesCSSValue | undefined | MakeStylesCSSObjectCustomL3;
+} & MakeStylesStrictCSSObject;
+type MakeStylesCSSObjectCustomL3 = {
+  [Property: string]: MakeStylesCSSValue | undefined | MakeStylesCSSObjectCustomL4;
+} & MakeStylesStrictCSSObject;
+type MakeStylesCSSObjectCustomL4 = {
+  [Property: string]: MakeStylesCSSValue | undefined | MakeStylesCSSObjectCustomL5;
+} & MakeStylesStrictCSSObject;
+type MakeStylesCSSObjectCustomL5 = {
+  [Property: string]: MakeStylesCSSValue | undefined;
+} & MakeStylesStrictCSSObject;
+
+export type MakeStylesAnimation = Record<'from' | 'to' | string, MakeStylesCSSObjectCustomL1>;
+export type MakeStylesStyle = MakeStylesStrictCSSObject | MakeStylesCSSObjectCustomL1;
 
 export type MakeStylesStyleFunctionRule<Tokens> = (tokens: Tokens) => MakeStylesStyle;
 export type MakeStylesStyleRule<Tokens> = MakeStylesStyle | MakeStylesStyleFunctionRule<Tokens>;
 
-export interface MakeStylesOptions {
-  dir: 'ltr' | 'rtl';
-  renderer: MakeStylesRenderer;
-}
-
-export type MakeStaticStylesStyle = {
-  [key: string]: CSS.Properties &
-    // TODO Questionable: how else would users target their own children?
-    Record<string, any>; // eslint-disable-line @typescript-eslint/no-explicit-any
-} & {
-  '@font-face'?: {
-    fontFamily: string;
-    src: string;
-
-    fontFeatureSettings?: string;
-    fontStretch?: string;
-    fontStyle?: string;
-    fontVariant?: string;
-    fontVariationSettings?: string;
-    fontWeight?: number | string;
-
-    unicodeRange?: string;
-  };
-};
-export type MakeStaticStyles = MakeStaticStylesStyle | string;
-
-export interface MakeStaticStylesOptions {
-  renderer: MakeStylesRenderer;
-}
-
-export interface MakeStylesRenderer {
-  id: string;
-
-  /**
-   * @private
-   */
-  insertionCache: Record<string, StyleBucketName>;
-
-  /**
-   * @private
-   */
-  styleElements: Partial<Record<StyleBucketName, HTMLStyleElement>>;
-
-  /**
-   * @private
-   */
-  insertCSSRules(cssRules: CSSRulesByBucket): void;
-}
-
-/**
- * Buckets under which we will group our stylesheets.
- */
-export type StyleBucketName =
-  // default
-  | 'd'
-  // link
-  | 'l'
-  // visited
-  | 'v'
-  // focus-within
-  | 'w'
-  // focus
-  | 'f'
-  // focus-visible
-  | 'i'
-  // hover
-  | 'h'
-  // active
-  | 'a'
-  // @keyframes definitions
-  | 'k'
-  // at-rules (@media, @support)
-  | 't';
-
 export type SequenceHash = string;
 export type PropertyHash = string;
-
-export type CSSClasses = /* ltrClassName */ string | [/* ltrClassName */ string, /* rtlClassName */ string];
-
-export type CSSClassesMap = Record<PropertyHash, CSSClasses>;
-export type CSSClassesMapBySlot<Slots extends string | number> = Record<Slots, CSSClassesMap>;
-
-export type CSSRulesByBucket = Partial<Record<StyleBucketName, string[]>>;
-
-export type StylesBySlots<Slots extends string | number, Tokens> = Record<Slots, MakeStylesStyleRule<Tokens>>;
-
-export type LookupItem = [/* definitions */ CSSClassesMap, /* dir */ 'rtl' | 'ltr'];
